c18631a5b5a897ed5d3ca686bf433b329a807cc0
Rishi <128126033+Rihiz@users.noreply.github.com>
2025-08-21 16:59:17 +0800

    Level 1 is done

--- src/main/java/Prometheus.java
+++ src/main/java/Prometheus.java
@@ -1,8 +1,26 @@
+import java.util.Scanner;
+
+
 public class Prometheus {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Prometheus");
         System.out.println("What can I do for you?");
-        System.out.println();
+        System.out.println("_____");
+
+
+        Scanner scanner = new Scanner(System.in);
+        String input;
+
+
+        while(true) {
+            input = scanner.nextLine();
+            System.out.println(input);
+            System.out.println("_____");
+            if (input.equalsIgnoreCase("bye")) {
+                break;
+            }
+        }
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
+
