a89615aaef356653b49fe3e6969428fdc7a67cf4
Rishi <128126033+Rihiz@users.noreply.github.com>
2025-09-16 20:28:17 +0800

    Improve UI design

--- src/main/java/prometheus/gui/Main.java
+++ src/main/java/prometheus/gui/Main.java
@@ -5,12 +5,12 @@ import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import prometheus.Prometheus;
 
 /**
- * A GUI for Duke using FXML.
+ * A GUI for Prometheus using FXML.
  */
 public class Main extends Application {
 
@@ -20,10 +20,13 @@ public class Main extends Application {
     public void start(Stage stage) {
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
-            AnchorPane ap = fxmlLoader.load();
-            Scene scene = new Scene(ap);
+            StackPane sp = fxmlLoader.load();
+            Scene scene = new Scene(sp);
             stage.setScene(scene);
-            fxmlLoader.<MainWindow>getController().setPrometheus(prometheus);  // inject the Duke instance
+            stage.setTitle("Prometheus Task Manager");
+            stage.setMinWidth(320);
+            stage.setMinHeight(400);
+            fxmlLoader.<MainWindow>getController().setPrometheus(prometheus);
             stage.show();
         } catch (IOException e) {
             e.printStackTrace();
